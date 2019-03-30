a1000c4fe12da0b78f33cfc0ef1fe6947db5333e
Marc-Olivier Mercier <marc-olivier.mercier@hpe.com>
2019-03-29 20:28:00 -0400

    Create map of all trips.

--- index.js
+++ index.js
@@ -15,7 +15,26 @@ request(requestSettings, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
         var nextPass = [];
-        feed.entity.forEach(function(entity) {
+        var allPass = {};
+        feed.entity.forEach((entity) => {
+            if (entity.trip_update && entity.trip_update.trip && entity.trip_update.trip.route_id && (entity.trip_update.trip.route_id === '72' || entity.trip_update.trip.route_id === '128')) {
+                if (!allPass[entity.trip_update.trip.route_id]) {
+                    allPass[entity.trip_update.trip.route_id] = {};
+                }
+                var route = allPass[entity.trip_update.trip.route_id];
+
+                var stops = entity.trip_update.stop_time_update;
+                if (stops) {
+                    stops.forEach(stop => {
+                        if (stop.arrival && stop.arrival.time) {
+                            if (!route[stop.stop_id]) {
+                                route[stop.stop_id] = [];
+                            }
+                            route[stop.stop_id].push(stop.arrival.time.low);
+                        }
+                    });
+                }
+            }
             if (entity.trip_update) {
                 if (entity.trip_update.trip.route_id === '72') {
                     var stops = entity.trip_update.stop_time_update;
@@ -27,6 +46,7 @@ request(requestSettings, function (error, response, body) {
                 }
             }
         });
+        console.log(JSON.stringify(allPass));
         var nextTime = nextPass.sort().map(x => x*1000 - Date.now()).filter(x => x > 0)[0];
         if (nextTime) {
             console.log(Math.floor(nextTime/(1000*60)) + ' minute(s)');
